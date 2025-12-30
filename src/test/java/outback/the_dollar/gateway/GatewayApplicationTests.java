9dcad2f383775e81c468cfae2856474c0336b72f
yoogahyeon <h_i_b_y_e@naver.com>
2025-12-30 22:01:40 +0900

    feat: gateway initial settings (#3)

---
+++ src/test/java/outback/the_dollar/gateway/GatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package outback.the_dollar.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
